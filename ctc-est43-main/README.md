50dacc5fa2c51db6c1e1686c27ffbc27883a5b43
ctcest43basket <ctcest43basket@gmail.com>
2026-01-08 11:38:22 +0100

    Delete ctc-est43-main directory

--- ctc-est43-main/README.md
+++
@@ -1,4 +0,0 @@
-# CTC EST43 Basket
-
-Site officiel de la CTC EST43 Basket.
-Projet prêt à être déployé sur Vercel.
--- ctc-est43-main/app/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>CTC EST43 Basket</h1>
-      <p>Site officiel de la CTC EST43 (Monistrol-sur-Loire & Sainte-Sigolène)</p>
-    </main>
-  );
-}
